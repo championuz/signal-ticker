8e4cfce0120ba6e6980599638ac10deb97ff3caa
Champion Uzoma <42459708+championuz@users.noreply.github.com>
2022-12-22 04:08:53 +0100

    update auth

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie: {
-    secure: true,
-    expires: new Date(Date.now() + 86400000)
+   expires: new Date(Date.now() + 86400000)
   }
 }));
 

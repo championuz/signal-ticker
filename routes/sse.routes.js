bb21aee1f77ed5d038f17098e191f2c9e1e5e27e
Champion Uzoma <42459708+championuz@users.noreply.github.com>
2022-12-26 09:20:38 +0100

    closed SSE

--- routes/sse.routes.js
+++ routes/sse.routes.js
@@ -3,6 +3,6 @@ const sseStream = require('../sse');
 
 const router = express.Router();
 
-router.get("/stream", sseStream.init)
+// router.get("/stream", sseStream.init)
 
 module.exports = router;
